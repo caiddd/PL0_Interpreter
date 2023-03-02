8889addc8c1ee73310d809a1aa9ea531a346819e
caidd <986897589@qq.com>
2023-03-02 05:48:16 -0800

    add test

--- format_count.sh
+++ format_count.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # 格式化
+clang-format -i `find include/ -type f -name *.h`
 clang-format -i `find include/ -type f -name *.hpp`
-clang-format -i `find src/ example/ test/ -type f -name *.cpp`
+clang-format -i `find src/ example/ test/ -type f -name *.cc`
 
 # 统计代码行数
 cloc --git `git branch --show-current`
